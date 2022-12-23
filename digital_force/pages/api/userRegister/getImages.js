fc48fd7e7fcae0a80923ed5b15bda225dd78b45d
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2022-12-23 14:30:45 +0530

    added user api in indexjs

--- digital_force/pages/api/userRegister/getImages.js
+++ digital_force/pages/api/userRegister/getImages.js
@@ -2,7 +2,13 @@ import User from "../../../models/User";
 import connectMongo from "../../../utils/connectMongo";
 
 export default async function getImages(req, res) {
-  const users = await User.find({}, { password: 0 }, { imgCollection: 1 });
+  console.log('req:', req)
+  console.log("Connecting To DB ");
+  await connectMongo();
+  console.log("Connected To DB ");
+  console.log("Fetcing  User From DB ");
+  const users = await User.find({ _id: id }, { imgCollection: 1 });
+  console.log("users:", users);
 
   if (!users) {
     return res.json({ error: true, message: "Users Not found" });
--- digital_force/pages/api/userRegister/getUser.js
+++ digital_force/pages/api/userRegister/getUser.js
@@ -5,6 +5,10 @@ const bcrypt = require("bcryptjs");
 const JWT_SECRET = "HACKATHON";
 
 export default async function getUser(req, res) {
+  console.log("Connecting To DB ");
+  await connectMongo();
+  console.log("Connected To DB ");
+  console.log("Fetcing  User From DB ");
   const users = await User.find({});
   if (!users) {
     return res.json({ error: true, message: "Users Not found" });
